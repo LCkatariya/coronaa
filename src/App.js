b48a66956248e55ba037b4efca88ea81bf572538
bababoss <suresheca2012@gmail.com>
2020-04-11 23:30:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,91 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+  import React, { Component } from 'react'
+  import './App.css'
+  
+  
+  class App extends Component {
+    constructor(props) {
+      super(props)
+      this.state = {
+        item:[],
+        isLoded: false,         
+        
+       
+      }
+    }
+  
+    componentDidMount(){
+      fetch('https://api.coronatracker.com/v3/stats/worldometer/country?countryCode=IN')
+  
+      .then(res=>res.json())
+      .then(json=>{
+        this.setState({
+          isLoded: true,
+          items: json,
+        })
+      });
+    }
+  
+   
+    render() {
+      
+     var {isLoded,items} = this.state;
+     
+  
+        if (!isLoded){
+          return<div>Loading....</div>;
+        }
+  
+      else{
+      return (
+        
+          <div className = "App">
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+            <div className = "box">
+              <div className="card">
+                <div className="container">
+                  <ul>
+                    {items.map(item=>(
+                      <li key = {item.id}>
+                      totalConfirmed:{item.totalConfirmed}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </div>        
+              <div className="card">
+                <div className="container">
+                  <ul>
+                    {items.map(item=>(
+                      <li key = {item.id}>
+                        totalRecovered:{item.totalRecovered}
+                      </li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+                <div className="card">
+                <div className="container">
+                  <ul>
+                    {items.map(item=>(
+                      <li key = {item.id}>
+                        totalDeaths:{item.totalDeaths}
+                      </li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+            </div>
+        
+
+          </div>
+          
+      )
+      }
+    }
+  }
+ 
+  
+  
+ 
 
 export default App;
